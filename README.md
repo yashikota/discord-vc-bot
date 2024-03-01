4dc2056569bab522106660429f62e77169fdb049
yashikota <52403688+yashikota@users.noreply.github.com>
2024-03-01 17:37:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# discord-vc-notification-bot
+
+![example](./example.png)
+
+## Usage
+
+Set the token, text channel ID and voice channel ID in `config.json`  
+For more information, see `config.example.json`  
---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { token, text_channel_id, voice_channel_id } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
+
+client.on(Events.VoiceStateUpdate, (_, newState) => {
+    const voiceChannel = newState.guild.channels.cache.get(voice_channel_id);
+
+    // Notification only if the first user joins the voice channel
+    if (voiceChannel.members.size !== 1) return;
+
+    if (newState.channelId === voice_channel_id) {
+        const textChannel = newState.guild.channels.cache.get(text_channel_id);
+
+        const embed = new EmbedBuilder()
+            .setColor(0x006e54)
+            .setTitle(`🔊 ${voiceChannel.name}`)
+        textChannel.send({ embeds: [embed] });
+    }
+});
