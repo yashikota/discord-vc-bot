7427163e908248aa6316ae6ca71d3221d9251b21
yashikota <52403688+yashikota@users.noreply.github.com>
2024-03-18 22:37:40 +0900

    fix voice channel join notification

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ client.once(Events.ClientReady, readyClient => {
 
 client.login(token);
 
-client.on(Events.VoiceStateUpdate, (_, newState) => {
+client.on(Events.VoiceStateUpdate, (oldState, newState) => {
     const voiceChannel = newState.guild.channels.cache.get(voice_channel_id);
+    if (!voiceChannel || voiceChannel.members.size !== 1) return;
 
-    // Notification only if the first user joins the voice channel
-    if (voiceChannel.members.size !== 1) return;
+    if (oldState.channelId === null && newState.channelId !== null) { // Join
+        if (newState.channelId === voice_channel_id) {
+            const textChannel = newState.guild.channels.cache.get(text_channel_id);
 
-    if (newState.channelId === voice_channel_id) {
-        const textChannel = newState.guild.channels.cache.get(text_channel_id);
-
-        const embed = new EmbedBuilder()
-            .setColor(0x006e54)
-            .setTitle(`🔊 ${voiceChannel.name}`)
-        textChannel.send({ embeds: [embed] });
+            const embed = new EmbedBuilder()
+                .setColor(0x006e54)
+                .setTitle(`🔊 ${voiceChannel.name}`)
+            textChannel.send({ embeds: [embed] });
+        }
     }
 });
